16e20f4cb9b09501b580fe238edaa37dcf4f597a
GoX1337 <gox.roche@gmail.com>
2016-02-04 23:42:15 +0100

    Add zombie line of sight (only left) + If player is spotted then we play sound

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 skelPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 rayOrig = new Vector2(transform.position.x - 0.2f, transform.position.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(rayOrig, Vector2.left);
+
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+
+            if (hit.collider.gameObject.name == "knightSprite_1")
+            {
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+                
+            }
+        }
+    }
+}
