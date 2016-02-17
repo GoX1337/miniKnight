649e2fc47352cfb661fc5f2513229139760ddf74
GoX1337 <gox.roche@gmail.com>
2016-02-17 18:45:43 +0100

    Game menu on escape key pressed

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    public Canvas menu;
+
+	// Use this for initialization
+	void Start () {
+        menu.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menu.enabled)
+            {
+                menu.enabled = false;
+                Time.timeScale = 1.0f;
+            }
+            else
+            {
+                menu.enabled = true;
+                Time.timeScale = 0f;
+            }
+        }
+	}
+}
