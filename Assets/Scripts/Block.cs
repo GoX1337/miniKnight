e38972723b671603e090cefa8882c716fffc088d
Mido <mahmoud.elbouhy@gmail.com>
2016-02-23 00:05:10 +0100

    add block (shield) animation + script + sound

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	private Animator animator;
+	private AudioSource audioSource;
+	public RandomSound shieldMetalSound;
+	public PlayerHealth hp;
+	public int damagePerAttack = 0;
+	public bool missed = true;
+
+	// Use this for initialization
+	void Start () {
+		this.animator = this.GetComponent<Animator>();
+		this.audioSource = GetComponent<AudioSource>();
+		hp = GameObject.FindObjectOfType<PlayerHealth> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (this.hp.isDead)
+			return;
+
+		if (Input.GetKey(KeyCode.LeftShift)){
+			this.missed = true;
+			this.animator.SetTrigger("block");
+			Debug.Log ("shield ON");
+		}
+	}
+
+	public void ShieldGameObject(GameObject gameObject)
+	{
+		if (gameObject.GetComponent<Health>() != null)
+		{
+			gameObject.GetComponent<Health>().TakeDamage(damagePerAttack);
+		}
+	}
+
+	public void ShieldAnimationEnd()
+	{
+		if (missed)
+		{
+			this.audioSource.PlayOneShot(this.shieldMetalSound.GetRandomSound());
+		}
+	
+	}
+}
---
+++ Assets/Scripts/ShieldBlock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldBlock : MonoBehaviour {
+	
+	private Block block;
+	private RandomSound wallHit;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		this.block = GetComponentInParent<Block>();
+		this.wallHit = GetComponent<RandomSound>();
+		this.audioSource = GetComponentInParent<AudioSource>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Walls")
+		{
+			this.block.missed = false;
+			this.audioSource.PlayOneShot(this.wallHit.GetRandomSound());
+		}
+		else if (other.name.Contains("Skeleton"))
+		{
+			this.block.missed = false;
+			this.block.ShieldGameObject(other.gameObject);
+		}
+	}
+}
