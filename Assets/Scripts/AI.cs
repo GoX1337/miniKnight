12ca4ff361fa0770af2122af7b8499c590ccc7e6
GoX1337 <gox.roche@gmail.com>
2016-02-22 21:12:02 +0100

    Change patrol direction if meet a corpse

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -58,7 +58,7 @@ public class AI : MonoBehaviour
 
         if (hitFall.collider != null)
         {
-            if (hitFall.collider.gameObject.name == "void")
+            if (hitFall.collider.gameObject.name == "void" || hitFall.collider.gameObject.name == "Skeleton")
             {
                 ChangePatrolDirection();
             }
